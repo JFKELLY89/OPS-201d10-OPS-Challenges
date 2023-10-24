1cb681aa4baf0ebe3f949fb0ab9e7f5e6518b0ec
John Kelly <jfkelly89@pm.me>
2023-10-24 12:31:08 -0700

    This is my first ops challenge

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script Name: Hello World
+# Author: John Kelly
+# Date of Last Revision: 10/24/2023
+# Purpose: OPS Challenge 1
+# Decleration of Variables
+message="I Am Working Perfectly"
+# Declaration of Funcations
+# Main
+echo "Hello World"
+echo $message
+# End
